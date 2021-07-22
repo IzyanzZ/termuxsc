3d733a02d4e86bf2e58d4f55857085bee2d18e71
IzyanzZ <87628633+IzyanzZ@users.noreply.github.com>
2021-07-22 21:18:22 +0700

    Update index.py

--- index.py
+++ index.py
@@ -3,6 +3,38 @@ import os,sys,time
 x = 'Username'
 y = 'Password'
 
+def mtk():
+  print("[1] Pertambahan\n[2] Pengurangan\n[3] Perkalian\n[4] Pembagian")
+  a = input("Pilih (1-4): ")
+  if a == 1:
+    ang = input("Angka 1: ")
+    ang2 = input("Angka 2: ")
+    total = ang + ang2
+    print("Hasil Pertambahan Tadi Adalah", total )
+    os.system("index.py")
+    time.sleep(3)
+  if a == 2:
+    anng = input("Angka 1: ")
+    anng2 = input("Angka 2: ")
+    total = anng - anng2
+    print("Hasil Pengurangan Tadi Adalah", total )
+    os.system("index.py")
+    time.sleep(3)
+  if a == 3:
+    annng = input("Angka 1: ")
+    annng2 = input("Angka 2: ")
+    total = annng * annng2
+    print("Hasil Perkalian Tadi Adalah", total )
+    os.system("index.py")
+    time.sleep(3)
+  if a == 1:
+    annnng = input("Angka 1: ")
+    annnng2 = input("Angka 2: ")
+    total = annnng + annnng2
+    print("Hasil Pembagian Tadi Adalah", total )
+    os.system("index.py")
+    time.sleep(3)
+
 def b():
   os.system("clear")
 
@@ -41,33 +73,3 @@ ins()
 jalan("  Pilih tools terlebih dahulu!  ".center(22))
 pil = input("Pilih (1/2): ")
 if pil == 1:
-  print("[1] Pertambahan\n[2] Pengurangan\n[3] Perkalian\n[4] Pembagian")
-  a = input("Pilih (1-4): ")
-  if a == 1:
-    ang = input("Angka 1: ")
-    ang2 = input("Angka 2: ")
-    total = ang + ang2
-    print("Hasil Pertambahan Tadi Adalah", total )
-    os.system("index.py")
-    time.sleep(3)
-  if a == 2:
-    anng = input("Angka 1: ")
-    anng2 = input("Angka 2: ")
-    total = anng - anng2
-    print("Hasil Pengurangan Tadi Adalah", total )
-    os.system("index.py")
-    time.sleep(3)
-  if a == 3:
-    annng = input("Angka 1: ")
-    annng2 = input("Angka 2: ")
-    total = annng * annng2
-    print("Hasil Perkalian Tadi Adalah", total )
-    os.system("index.py")
-    time.sleep(3)
-  if a == 1:
-    annnng = input("Angka 1: ")
-    annnng2 = input("Angka 2: ")
-    total = annnng + annnng2
-    print("Hasil Pembagian Tadi Adalah", total )
-    os.system("index.py")
-    time.sleep(3)
